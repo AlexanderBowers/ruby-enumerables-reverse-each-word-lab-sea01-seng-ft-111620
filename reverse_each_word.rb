1799f4e5fd50acf2a6f47c880ca4355fdb8b2da8
Alexander Bowers <7319abowers@gmail.com>
2020-10-14 05:53:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 require 'pry'
 def reverse_each_word(string)
+  reverse_string = ""
    my_array = string
    my_array = my_array.split
    #binding.pry
    my_array.map do |reverse|
      reverse.reverse
-
    end
+
    #my_array.to_s
 end
